af165ad2283090336ad4161769c17cf8e34ce8f8
unknown <sois.milindsir@gmail.com>
2021-07-30 22:18:33 +0530

    Backend Server creation

---
+++ v1/milind/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express');
+const mongoose = require("mongoose");
+const port = process.env.PORT || 3000;
+const app = express();
+
+//initializing of mongoDb
+mongoose.connect(process.env.MONGO_URL, {
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+    
+});
+
+mongoose.connection.once('open', (err) => {
+    console.log( err || 'connected to database...')
+   
+})
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.listen(port, () =>{
+    console.log(`server is running on port ${port}`)
+})
+
