30b63d25bf6d0e3fd7366b69c7b39d4b2b9cadf0
starbvuks <starbvuks@gmail.com>
2021-07-30 11:51:02 +0530

    logo-nav

--- v1/landmark-loc/src/components/Navbar/styled.Navbar.js
+++ v1/landmark-loc/src/components/Navbar/styled.Navbar.js
@@ -2,18 +2,19 @@ import styled from "styled-components";
 import {AppBar, Toolbar, IconButton} from "@material-ui/core";
 
 export const StyledIconButtonLogo = styled(IconButton)`
-  width: 9vw;
+  width: 10vw;
+  height: 64px;
+  display: flex;
 `;
 
 export const Icon = styled.img`
-  display: flex;
-  margin: -12px 0 0 20px;
+  margin: -2vh 0 0 0;
   width: 60%;
 `;
 
 export const StyledAppBar = styled(AppBar)`
   background-color: #a6566b !important;
-  width: 90% !important;
+  width: 90vw !important;
 `;
 
 export const StyledToolbar = styled(Toolbar)`
