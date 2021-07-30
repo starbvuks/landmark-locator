3952566bb93350da2ef65189f292eb1a272284c2
starbvuks <starbvuks@gmail.com>
2021-07-30 12:04:35 +0530

    basic-home-finish

--- v1/landmark-loc/src/components/Home/Home.jsx
+++ v1/landmark-loc/src/components/Home/Home.jsx
@@ -4,23 +4,30 @@ import styled from "styled-components";
 const Home = () => {
   return (
     <Main>
-      <CenterText>
-        <Shell src="./img/ii-iconfull.png" alt="icon" />
-        {/* <Text>
+      <Center>
+        <CenterText>
+          <Shell src="./img/ii-iconfull.png" alt="icon" />
+          {/* <Text>
           <TextTop>Landmark</TextTop>
           <TextBot>Locator</TextBot>
         </Text> */}
-      </CenterText>
-      <CenterIcon>
-        <MapIcon src="./img/main-map.png" alt="icon" />
-      </CenterIcon>
+        </CenterText>
+        <CenterIcon>
+          <MapIcon src="./img/main-map.png" alt="icon" />
+        </CenterIcon>
+      </Center>
+      <ArrowIconDiv>
+        <ArrowIcon src="./img/drop-arrow.png" alt="icon" />
+      </ArrowIconDiv>
     </Main>
   );
 };
 
 export default Home;
 
-const Main = styled.div`
+const Main = styled.div``;
+
+const Center = styled.div`
   display: flex;
   align-content: center;
   justify-content: space-between;
@@ -66,4 +73,14 @@ const CenterIcon = styled.div`
 
 const MapIcon = styled.img`
   width: 25vw;
+`;
+
+const ArrowIconDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 8vh 0 0 0;
+`;
+
+const ArrowIcon = styled.img`
+  width: 5vw;
 `;
--- v1/landmark-loc/src/components/Navbar/Navbar.jsx
+++ v1/landmark-loc/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import MapIcon from "@material-ui/icons/Map";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
 import {
