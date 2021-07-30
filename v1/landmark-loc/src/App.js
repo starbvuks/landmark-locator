58b59bc33a148027e3a1bf45b3f0aeaff280a045
starbvuks <starbvuks@gmail.com>
2021-07-30 18:46:20 +0530

    marker-long/lat

--- v1/landmark-loc/src/App.js
+++ v1/landmark-loc/src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 //import styled from "styled-components";
 
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./components/Home/Home";
+import Navbar from "./components/Landing/Navbar/Navbar";
+import Home from "./components/Landing/Home/Home";
 import Map from "./components/Map/Map";
 
 function App() {
--- v1/landmark-loc/src/components/Map/Map.jsx
+++ v1/landmark-loc/src/components/Map/Map.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import {useState} from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, {Marker} from "react-map-gl";
 import styled from "styled-components";
-
+import RoomIcon from "@material-ui/icons/Room";
 const MAPBOX_TOKEN =
   "pk.eyJ1Ijoic3RhcmJ2dWtzIiwiYSI6ImNrbTR2NHphaDA4ZmUzMHBtMTRobjN6MHMifQ.tmPNZOallU2bty-vG7qIdw";
 
 const Map = () => {
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 13.684487872955962,
+    width: 500,
+    height: 500,
     longitude: 79.3473141412759,
+    latitude: 13.684487872955962,
     zoom: 8,
   });
+
+  console.log(viewport.latitude);
+
+  const markers = (
+    <Marker longitude={79.3473141412759} latitude={13.684487872955962}>
+      <RoomIcon fontSize="medium" />
+    </Marker>
+  );
 
   return (
     <MainDiv>
@@ -23,7 +31,9 @@ const Map = () => {
           mapStyle="mapbox://styles/mapbox/streets-v11"
           mapboxApiAccessToken={MAPBOX_TOKEN}
           onViewportChange={(nextViewport) => setViewport(nextViewport)}
-        />
+        >
+          {markers}
+        </ReactMapGL>
       </MapDiv>
     </MainDiv>
   );
