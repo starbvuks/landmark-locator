8407734e202f3b3b0e9c8cd5c2996662ef2257af
teja548 <tet9692@gmail.com>
2021-08-02 13:30:53 +0530

    admin

--- v1/landmark-loc/src/App.js
+++ v1/landmark-loc/src/App.js
@@ -1,27 +1,30 @@
-import "./App.css";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-//import styled from "styled-components";
-
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./components/Home/Home";
-import Map from "./components/Map/Map";
+import React from 'react'
+import { Admin, Resource } from 'react-admin'
+import restProvider from 'ra-data-simple-rest'
+import PostList from './components/PostList'
+import PostCreate from './components/PostCreate'
+import PostEdit from './components/PostEdit'
+import UserList from './components/UserList'
+import UserCreate from './components/UserCreate'
+import UserEdit from './components/UserEdit'
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/map">
-            <Map />
-          </Route>
-          <Route path="/">
-            <Navbar />
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+    <Admin dataProvider={restProvider('http://localhost:3000')}>
+      <Resource
+        name='posts'
+        list={PostList}
+        create={PostCreate}
+        edit={PostEdit}
+      />
+      <Resource
+        name='users'
+        list={UserList}
+        create={UserCreate}
+        edit={UserEdit}
+      />
+    </Admin>
+  )
 }
 
-export default App;
+export default App
