c82c09223884f9257887db2abd976315baad3197
starbvuks <starbvuks@gmail.com>
2021-08-05 15:36:51 +0530

    simple headers fix

--- v1/milind/range.js
+++ v1/milind/range.js
@@ -1,4 +1,5 @@
 module.exports = (req, res, next) => {
-    res.header('Content-Range', 'posts 0-20/20');
-    next();
-}
+  res.header("Access-Control-Expose-Headers", "Content-Range");
+  res.header("Content-Range", "posts 0-20/20");
+  next();
+};
--- v1/milind/server.js
+++ v1/milind/server.js
@@ -1,41 +1,40 @@
-require('dotenv').config()
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 3000;
-const adminRoute = require('./routes/adminroutes')
-const userRoute = require('./routes/userRoute')
-const landmarRoute = require('./routes/landmarkRouter')
-const ratingRoute = require('./routes/ratingRoutes');
+const adminRoute = require("./routes/adminroutes");
+const userRoute = require("./routes/userRoute");
+const landmarRoute = require("./routes/landmarkRouter");
+const ratingRoute = require("./routes/ratingRoutes");
+const range = require("./range");
 const app = express();
 
 //initializing of mongoDb
 mongoose.connect(process.env.MONGO_URL, {
-    useCreateIndex:true,
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-    
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-mongoose.connection.once('open', (err) => {
-    console.log( err || 'connected to database...')
-   
-})
+mongoose.connection.once("open", (err) => {
+  console.log(err || "connected to database...");
+});
 
 //middleware
+app.use(range);
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({extended: false}));
 app.use(cors());
 
-app.use('/admin', adminRoute);
-app.use('/user', userRoute)
-app.use('/landmark', landmarRoute);
-app.use('/rating', ratingRoute);
-app.get('/', (req,res) => {
-    res.send('welcome to landmark locator')
-})
+app.use("/admin", adminRoute);
+app.use("/user", userRoute);
+app.use("/landmark", landmarRoute);
+app.use("/rating", ratingRoute);
+app.get("/", (req, res) => {
+  res.send("welcome to landmark locator");
+});
 
-app.listen(port, () =>{
-    console.log(`server is running on port ${port}`)
-})
-
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
